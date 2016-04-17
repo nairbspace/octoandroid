811ba2e71a1be2e8ed50e1cda5e05b5b54e618d7
nairbspace <nairbspace@users.noreply.github.com>
2016-04-17 05:06:37 -0700

    Project created using BlankActivity template.

---
+++ app/src/main/java/com/nairbspace/octoandroid/AddPrinterActivity.java
@@ -0,0 +1,13 @@
+package com.nairbspace.octoandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddPrinterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_printer);
+    }
+}
